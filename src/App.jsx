c957ad123a1c873475b2acab804dcbb3e4ecebe6
Verkkopantteri <verkkopantteri@gmail.com>
2026-06-02 02:21:46 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -85,7 +85,7 @@ export default function App() {
             }}
             aria-label="Toggle theme"
           >
-            {darkMode ? "☀️" : "🌙"}
+            {darkMode ? "☼" : "☽"}
           </button>
         </div>
 
